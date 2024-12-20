227b634223c50ae660441fc1e36a23587a496d13
SDG223157 <isky999@gmail.com>
2024-12-20 15:03:34 +0800

    Your commit message here

--- app/analyzer/stock_analyzer.py
+++ app/analyzer/stock_analyzer.py
@@ -65,7 +65,7 @@ def create_stock_visualization(
         analysis_df = AnalysisService.analyze_stock_data(historical_data, crossover_days)
         
         # Perform regression analysis
-        regression_results = AnalysisService.perform_polynomial_regression(historical_data)
+        regression_results = AnalysisService.perform_polynomial_regression(historical_data, future_days=180)
         
         # Find crossover points
         crossover_data = AnalysisService.find_crossover_points(
